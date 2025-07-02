212e3769208152a1a8f5e85317c8211a26ed2c2d
Mahammad Farajov14 <mahammadfarajov1029876543@gmail.com>
2025-07-02 18:45:15 +0400

    changes

--- grammar/settings.py
+++ grammar/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'django_filters',
     "corsheaders",
     "users",
+    'auth_service',
 ]
 
 
--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ echo "👤 Creating superuser if not exists..."
 python create_superuser.py
 
 echo "🚀 Starting Celery worker in background..."
-celery -A auth_service worker --loglevel=info &
+celery -A grammar worker --loglevel=info &
 
 echo "🌐 Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
